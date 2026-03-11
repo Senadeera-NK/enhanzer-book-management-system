8916ea8f4809f9358842cd9a0755ab8db5d771f9
Senadeera NK <naduni.20191206@iit.ac.lk>
2026-03-12 04:33:00 +0530

    feat: initialized save book form components

--- frontend/src/app/components/book-list/book-list.ts
+++ frontend/src/app/components/book-list/book-list.ts
@@ -2,18 +2,34 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core'; // 1. Add 
 import { CommonModule } from '@angular/common';
 import { Book } from '../../models/book';
 import { BookService } from '../../services/bookService';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './book-list.html',
   styleUrl: './book-list.css',
 })
 export class BookList implements OnInit {
   books: Book[] = [];
 
+  //for the save book modal
+  showModal = false;
+
+  //new book object - save book
+  newBook:Book ={
+    title:'',
+    author:'',
+    isbn:'',
+    publicationDate:''
+  };
+
   constructor(private bookService: BookService, private cdr: ChangeDetectorRef) {}
+
+  toggleModal(){
+    this.showModal = !this.showModal;
+  }
 
   ngOnInit(): void {
     this.loadBooks();
@@ -34,4 +50,17 @@ export class BookList implements OnInit {
   onAddClick() {
     alert("Time to add a new book! (We'll build the form when you wake up!)");
   }
+
+  //to save books
+  saveBook(){
+    this.bookService.addBook(this.newBook).subscribe({
+      next:(result)=>{
+        this.loadBooks();
+        this.toggleModal();
+        this.newBook = {title:'',author:'',isbn:'',publicationDate:''};
+      },
+      error:(err)=>alert("failed to add book")
+    })
+  }
+
 }
