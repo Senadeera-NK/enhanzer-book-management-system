3d16b6fcf4dc1d5f94826040002efcd824488374
Senadeera NK <naduni.20191206@iit.ac.lk>
2026-03-12 03:04:20 +0530

    feat: initialized the booklist to the app

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BookList } from './components/book-list/book-list'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [RouterOutlet, BookList], 
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class AppComponent {
+  title = 'frontend';
 }
--- frontend/src/app/components/book-list/book-list.ts
+++ frontend/src/app/components/book-list/book-list.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {Book} from '../../models/book';
+import { BookService } from '../../services/bookService';
 @Component({
   selector: 'app-book-list',
   imports: [],
   templateUrl: './book-list.html',
   styleUrl: './book-list.css',
 })
-export class BookList {}
+export class BookList implements OnInit{
+  books:Book[]=[];
+
+  constructor(private bookService:BookService){}
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks():void{
+    this.bookService.getBooks().subscribe({
+      next:(data)=>this.books=data,
+      error:(err)=>console.error('Error fetching books',err)
+    });
+  }
+}
