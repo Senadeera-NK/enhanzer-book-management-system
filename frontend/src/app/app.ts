94c5870c862e431f7fb59bd203f960daa576d319
Senadeera NK <naduni.20191206@iit.ac.lk>
2026-03-12 05:44:43 +0530

    chore: updated the app.ts by removing unnecessary logs

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,12 @@
-// src/app/app.ts
 import { Component } from '@angular/core';
-import { BookList } from './components/book-list/book-list'; // Match your actual filename
+import { BookList } from './components/book-list/book-list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [BookList], // <--- YOU MUST IMPORT IT HERE
+  imports: [BookList],
   template: `
-    <h1>📚 Enahnzer Book Management</h1>
+    <h1>Enahnzer Book Management</h1>
     <p>Software Engineer Assignment - March 2026</p>
     <hr>
     <app-book-list></app-book-list> 
